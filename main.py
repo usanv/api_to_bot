5a8797b3e7d5e5d98eaf242000a8916dcab17ca8
usanv <140505776+usanv@users.noreply.github.com>
2023-09-15 18:09:38 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def on_shutdown(dp):
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     tasks = [
-        loop.create_task(uvicorn.run(api.app, host="127.0.0.1", port=8000)),
+        loop.create_task(uvicorn.run(api.app, host="0.0.0.0", port=8000)),
         loop.create_task(bot.dp.start_polling(bot, on_shutdown=on_shutdown))
     ]
     loop.run_until_complete(asyncio.gather(*tasks))
