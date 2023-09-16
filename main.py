252421dd1acde5abe7a589cb40cebc3a47c83e0c
usanv <140505776+usanv@users.noreply.github.com>
2023-09-16 18:04:07 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -19,14 +19,14 @@
 
 
 import asyncio
-import os
-
 import uvicorn
+import os
+from fastapi import FastAPI
 from aiogram import Dispatcher, Bot
 from dotenv import load_dotenv, find_dotenv
-from fastapi import FastAPI
 
 load_dotenv(find_dotenv())
+from pathlib import Path
 
 # Создаем экземпляр FastAPI
 app = FastAPI()
@@ -61,7 +61,7 @@ async def on_shutdown(dp):
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     tasks = [
-        loop.create_task(uvicorn.run(app='__main__:app', workers=2, reload=True)),
+        loop.create_task(uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, workers=2)),
         loop.create_task(dp.start_polling(bot, on_shutdown=on_shutdown))
     ]
     loop.run_until_complete(asyncio.gather(*tasks))
